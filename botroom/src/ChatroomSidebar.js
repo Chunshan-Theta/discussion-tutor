ae9e6ac3f1268d267a3276762f92661e9dfab347
Wang <gavin19950511@gmail.com>
2023-12-27 13:08:59 +0000

    update(web): update page

--- botroom/src/ChatroomSidebar.js
+++ botroom/src/ChatroomSidebar.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 const ChatroomSidebar = () => {
   const tips = [
-    { id: 1, title: '歡迎來到文字冒險遊戲' , content: '請透過聊天室進行遊玩，先跟我說個`hi`來開始吧！' },
+    { id: 1, title: '歡迎來到論證活動前導機器人' , content: '請透過聊天室來進行活動前導，先跟我說個`hi`來開始吧！' },
     // Add more rooms as needed
   ];
   
   return (
     <div className="sidebar">
-      <h2>Chat Step</h2>
+      <h2>論證活動前導機器人</h2>
+      <h2>論證活動題目：<p style={{color: '#aa3333'}}>請問核能發電是不是胎灣應該要發展的目標呢？</p></h2>
       <ul>
         {tips.map(tip => (
           <li>
