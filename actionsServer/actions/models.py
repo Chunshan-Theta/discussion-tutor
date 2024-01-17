b2b9569488a231a542ec97b4649f6e2eefa1b456
Wang <gavin19950511@gmail.com>
2024-01-17 00:18:04 +0000

    update(stage): update stage statement

--- actionsServer/actions/models.py
+++ actionsServer/actions/models.py
@@ -39,69 +39,35 @@ assert(checkClient(client))
 
 
 
+
 #
+def genSystemPrompt(stage: Stage):
+    systemPrompt = stage.situation["role"]+stage.situation["task"]+"\n\nplease Reply base on you know.\n you know:\n"
+    for unit in stage.target['rag']:
+        systemPrompt += "- "+unit[0]+"?"+unit[1]
+        systemPrompt += "\n"
+    return systemPrompt+"\n keep reply simply with english"
+
+
 def callGPTByStage(userId: str, Stype: str, userText: str) -> str:
     
-    if Stype == "intro_bot":
-        stage = stage_intro_bot
-    elif Stype == "intro_unclear_power":
-        stage = stage_intro_unclear_power
-    elif Stype == "intro_discussion":
-        stage = stage_intro_discussion
-    elif Stype == "intro_ask":
-        stage = stage_try_ask
-
-    elif Stype == "intro_reply":
-        stage = stage_try_reply
-
-    else:
-        return "callGPTByStage Done."
+    stage = getStage(Stype)
 
     botReply: str = ""
-    if stage.system == "reply/ans":
-        systemPrompt = stage.situation["role"]+stage.situation["task"]+"\n\nplease Reply base on you know.\n you know:\n"
-        for unit in stage.target['rag']:
-            systemPrompt += "- "+unit[0]+"?"+unit[1]
-            systemPrompt += "\n"
-        
-        prompts = [{
-            "role": "system",
-            "content": systemPrompt+"\n 請用中文;zh-tw回應;且回應的篇幅必須簡短"
-        }]
-        prompts.append({
-                        "role": "user",
-                        "content": userText
-                    })
-        # botReply += "\n***"+"\n***".join([str(unit) for unit in prompts])
-        botReply += "\n"+callGpt(prompts, 0.3)
-    elif stage.system == "practice":
-        prompts = [{
+    systemPrompt = genSystemPrompt(stage)
+    
+    
+    prompts = [{
         "role": "system",
-        "content": stage.situation["role"]+stage.situation["task"]
-        }]
-        for unit in stage.target['rag']:
-            prompts.append({
-                        "role": "assistant",
-                        "content": "\n".join(stage.action["toAgent"])
-                    })
-            prompts.append({
-                        "role": "user",
-                        "content": unit[0]
-                    })
-            prompts.append({
-                        "role": "assistant",
-                        "content": unit[1]
-                    })
-        prompts.append({
-                        "role": "assistant",
-                        "content": "\n".join(stage.action["toAgent"])
-                    })
-        prompts.append({
-                        "role": "user",
-                        "content": userText
-                    })
-        # botReply += "\n***"+"\n***".join([str(unit) for unit in prompts])
-        botReply += "\n"+callGpt(prompts, 0.3)
+        "content": systemPrompt
+    }]
+    prompts.append({
+                    "role": "user",
+                    "content": userText
+                })
+    # botReply += "\n***"+"\n***".join([str(unit) for unit in prompts])
+    botReply += "\n"+callGpt(prompts, 0.3)
+
     if "continuer" in stage.action:
         botReply +="\n"+stage.action["continuer"]
     return botReply       
